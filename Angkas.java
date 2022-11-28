f7acb7b0eda935c97617880a6b3a5844681a80d2
ixidrosolon17 <20400020@usc.edu.ph>
2022-11-28 19:00:56 +0800

    [ixidrosolon17]Changed number of variables to 1000

--- Angkas.java
+++ Angkas.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Angkas {
 	
-	private static final int arraySize = 100;
+	private static final int arraySize = 1000;
 	
 	private static Location[] locations = new Location[arraySize];
 	private static Driver[] drivers = new Driver[arraySize];
