02fdffef958bde9fbbaaebe44ab50199ec6d3592
Arthur <18104362@usc.edu.ph>
2022-11-29 19:37:56 +0800

    Added Comments to the Admin File

--- Admin.java
+++ Admin.java
@@ -1,5 +1,6 @@
 public class Admin extends User {
-	
+	//Class for Admin
+	//Displays the Admin with name and Mobile Number 
 	public String toString() {
 		return "[Admin] " + name + ", " + mobileNumber;
 	}
