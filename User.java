4c54546103a22b101431933da4a18f36a2777830
Mark Balili <markycahayagan@gmail.com>
2022-11-29 14:18:04 +0800

    [adjustment][Mark Balili] Added a comment to User.java

--- User.java
+++ User.java
@@ -13,7 +13,7 @@ public class User {
 		this.mobileNumber = mobileNumber;
 		this.SOScontactNumber = SOScontactNumber;
 	}
-	
+	//Setters and getters
 	public String getName() {
 		return this.name;
 	}
