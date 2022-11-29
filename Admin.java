57cc5e29a8019cef9407ee50ef38946dde4078bd
unknown <jaydegamo22@gmail.com>
2022-11-29 20:37:55 +0800

    [comment][JDegamo] modified Admin.java, added comments

--- Admin.java
+++ Admin.java
@@ -1,4 +1,6 @@
 public class Admin extends User {
+	//Jay Degamo
+	//Git Exercise CPE 3121
 	//public class for admins
 	//Displays the Admin with name and Mobile Number 
 	public String toString() {
