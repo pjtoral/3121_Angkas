59c2ffba66d43e0a1ebec37d14c092d00721529c
unknown <jaydegamo22@gmail.com>
2022-11-29 20:36:05 +0800

    [comment][JDegamo] committed a comment

--- Admin.java
+++ Admin.java
@@ -1,5 +1,5 @@
 public class Admin extends User {
-	//Class for Admins
+	//public class for admins
 	//Displays the Admin with name and Mobile Number 
 	public String toString() {
 		return "[Admin] " + name + ", " + mobileNumber;
